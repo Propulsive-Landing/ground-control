157ab1c026db9031f769249382a34309f84f0c5f
Jack Wagner <john.wagner@uconn.edu>
2021-10-17 14:11:16 -0400

    Jack trials

--- 3D_plotting.py
+++ 3D_plotting.py
@@ -18,3 +18,14 @@ graph.set_zlim([0,10])
 graph.scatter3D(0, 0, 0, color = 'b')
 plt.show()
 
+plt.figure()                                    #tried to display two graphs
+graph = plt.axes(projection = '3d')             #first graph above popped up 
+graph.set_xlabel('z-coordinate')                #As soon as closed the first graph, second one popped up
+graph.set_ylabel('y-coordinate')                #appear to not show two seperate graphs at the same time
+graph.set_zlabel('z-coordinate')
+graph.autoscale(False)                          #copy and pasting this code (left) into another window and trying to run both 
+graph.set_xlim([-5, 5])                         #at the same time also failed, one than the other
+graph.set_ylim([-5, 5])
+graph.set_zlim([0,10])
+graph.scatter3D(1, 1, 1, color = 'b')
+plt.show()
