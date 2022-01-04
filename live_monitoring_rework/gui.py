4365d43735c5220e7cbfa8b44b9ef2ec799edc9c
gabekole <gabriel.koleszar@uconn.edu>
2022-01-04 17:06:49 -0500

    Update gui.py

--- live_monitoring_rework/gui.py
+++ live_monitoring_rework/gui.py
@@ -1,6 +1,9 @@
 import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
+import pyqtgraph as pg
+
+#https://www.pythonguis.com/tutorials/plotting-pyqtgraph/
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self) -> None:
@@ -11,9 +14,16 @@ class MyWidget(QtWidgets.QWidget):
         self.button = QtWidgets.QPushButton("CLICK ME!")
         self.text = QtWidgets.QLabel("HELLO", alignment=QtCore.Qt.AlignCenter)
 
+        self.graph = pg.PlotWidget()
+
         self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.graph)
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
+
+        hour = [1,2,3,4,5,6,7,8,9,10]
+        temperature = [30,32,34,32,33,31,29,32,35,45]
+        self.graph.plot(hour, temperature)
 
         self.button.clicked.connect(self.magic)
 
