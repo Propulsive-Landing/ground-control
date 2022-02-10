86b82d364b840d6e0810f47617526f6450bcad52
gabekole <gabriel.koleszar@uconn.edu>
2022-02-10 15:08:32 -0500

    Reworked gui now main gui

--- live_monitoring/rf.py
+++ live_monitoring/rf.py
@@ -1,55 +1,89 @@
-from multiprocessing import Queue, Array
+from multiprocessing import Queue, Array, Value, Process, Pipe
+from multiprocessing.connection import PipeConnection
+from threading import Thread
+from time import sleep
 
 import serial
-import tkinter as tk
 from struct import *
 
 
-
-
-
 class RF():
-    def __init__(self, port, baud, backlog_threshold = 6000, telem_string='=IiffffI', telem_attibute_num=7):
-        self._comport = serial.Serial(port, baud)
-        self._backlog_bytes_num = backlog_threshold #how many bytes to be in list for a backlog
+    def __init__(self, port : str, baud : int, current_value: Array, telem_frame_queue: Queue, log_queue: Queue, handled_most_recent : Value, backlog_threshold = 6000, telem_string='=IiffffI'):
+
+        self.port = port
+        self.baud = baud
 
         self._bytes_received = [] #This list holds recieved bytes and this list is searched through to find packets
         self._history = []
-        self._TELEM_HEADER =  [239, 190, 173, 222] #4 byte heaeder to indicate telem frame, 0xDEADBEEF
-        self._STRING_HEADER = [206, 250, 186, 186] #4 byte header to indicate string data, 0xBABAFACE
-        self._FOOTER = 3405707998 #4 byte uint32_t to indicate ending of string or telem frame // 202, 254, 250, 222
 
-        self._telem_struct = telem_string
-        self._sizeofstruct = calcsize(self._telem_struct)
+        self._delay_between_packets = 5 #Delay (in ms) if there is not enough data in input buffer to be read into something meaningful
 
+        self._transmittion_constants = {
+            'TELEM_HEADER': [239, 190, 173, 222], #4 byte heaeder to indicate start of telem frame, 0xDEADBEEF
+            'STRING_HEADER': [206, 250, 186, 186], #4 byte header to indicate start of string data, 0xBABAFACE
+            'FOOTER': 3405707998, #4 byte value to indicate ending of string or telem frame 0xCAFEFADE
+            'backlog_threshold' : backlog_threshold #how many bytes to be in list for a backlog
+        }
+
+        self._telem_struct_unpacking_values = {
+            'telem_struct_string': telem_string,
+            'size_of_telem_struct': calcsize(telem_string)
+        }
+
+        self.input_receiver, self.input_transmitter = Pipe(duplex=False)
+
+        self.handled_most_recent = handled_most_recent
+
+        self._current_telem_frame = current_value
+        self._telem_frame_queue = telem_frame_queue
+        self._log_queue = log_queue
+
+    def connect_serial(self, port: str, baud: int) -> bool:
+        try:
+            self._comport = serial.Serial(port, baud)
+            self._comport.reset_input_buffer()
+            return True
+        except serial.SerialException:
+            return False
+
+    def disconnect_serial(self):
+        try:
+            self._comport.close()
+            return True
+        except ValueError:
+            return False
+
+    def _write_loop(self, running, receiver : PipeConnection):
+        while(running.value == 1):
+            if(receiver.poll(timeout=1)):
+                val = receiver.recv()
+                self._comport.write(bytes(val))
+
+    def _listen_loop(self, running: Value):
+        self._comport = serial.Serial(self.port, self.baud)
         self._comport.reset_input_buffer()
 
+        self.write_thread = Thread(target=self._write_loop, args=(running,self.input_receiver))
 
-        self.telem_frame_queue = Queue()
-        self.log_queue = Queue()
-
-        self.current_telem_frame = Array("d", telem_attibute_num)
-
-    def close(self):
+        while(running.value == 1):
+            self.read_binary()
+        
         self._comport.close()
 
-    def handle_string_received(self, str):
-        self.log_queue.put(str)
-
-    def handle_string_error(self, str):
-        self.log_queue.put(str)
-
-    def handle_telem_error(self, error_msg, frame):
-        self.log_queue.put(error_msg)
-        self.log_queue.put(frame)
+    #Starts a separate process that will connect serial port then listen for data
+    def start_listen_loop(self, running: Value):
+        process = Process(target=self._listen_loop, args=(running,), name="Data Search Loop")
+        process.start()
 
     def handle_alignment_notice(self, bytes_skipped):
-        self.log_queue.put(str(bytes_skipped) + " required to align")
+        self._log_queue.put(str(bytes_skipped) + " required to align")
 
 
     #Should be called with high frequency to ensure propper readings
     def read_binary(self):
-        if(self._comport.in_waiting >= 5): #waits until there is data of at least the size of the struct because if there is not that much data, there cannot be a complete struct
+        if(self._comport.in_waiting < 5):
+            sleep(.005)
+        else: #waits until there is data of at least the size of the struct because if there is not that much data, there cannot be a complete struct
             
             received = self._comport.read(self._comport.in_waiting) #reads all available data from input buffer into bytearray
             self._bytes_received.extend(list(received)) #adds recieved data to a list
@@ -57,40 +91,41 @@ class RF():
             
             #print(self._bytes_received)
 
-            if(len(self._bytes_received) > self._backlog_bytes_num):
+            if(len(self._bytes_received) > self._transmittion_constants['backlog_threshold']):
                 #Backlog condition
                 self._bytes_received.clear()
-                self.log_queue.put("backlog")
+                self._log_queue.put("backlog")
 
             if(len(self._bytes_received) < 10):
                 return
-            if(self._bytes_received[0:4] != self._TELEM_HEADER and self._bytes_received[0:4] != self._STRING_HEADER): #if the first four bytes received do not match the struct magic number, then alignment must be done
+            if(self._bytes_received[0:4] != self._transmittion_constants['TELEM_HEADER'] and self._bytes_received[0:4] != self._transmittion_constants['STRING_HEADER']): #if the first four bytes received do not match the struct magic number, then alignment must be done
                 stra = f'aligned: {self._bytes_received} '
                 
                 iterations = 0
-                while(len(self._bytes_received) >= 4 and (self._bytes_received[0:4] != self._TELEM_HEADER and self._bytes_received[0:4] != self._STRING_HEADER)): #removes from the front of the struct until a head is found
+                while(len(self._bytes_received) >= 4 and (self._bytes_received[0:4] != self._transmittion_constants['TELEM_HEADER'] and self._bytes_received[0:4] != self._transmittion_constants['STRING_HEADER'])): #removes from the front of the struct until a head is found
                     self._bytes_received.pop(0)
                     iterations += 1
-                stra += f'{iterations} byes'
-                self.log_queue.put(stra)
+                stra += f'{iterations} bytes'
+                self._log_queue.put(stra)
 
             if(len(self._bytes_received) < 4):
                 return
 
-            if((self._bytes_received[0:4] == self._TELEM_HEADER and len(self._bytes_received) >= self._sizeofstruct)): #When there is a valid header and enough bytes are in the list, data is then read.
-                res = bytearray(self._bytes_received[0:self._sizeofstruct])
-                del self._bytes_received[0:self._sizeofstruct]
-                frame = unpack(self._telem_struct, res)
+            if((self._bytes_received[0:4] == self._transmittion_constants['TELEM_HEADER'] and len(self._bytes_received) >= self._telem_struct_unpacking_values['size_of_telem_struct'])): #When there is a valid header and enough bytes are in the list, data is then read.
+                res = bytearray(self._bytes_received[0:self._telem_struct_unpacking_values['size_of_telem_struct']])
+                del self._bytes_received[0:self._telem_struct_unpacking_values['size_of_telem_struct']]
+                frame = unpack(self._telem_struct_unpacking_values['telem_struct_string'], res)
                 
                 
-                if(frame[-1:][0] != self._FOOTER):
-                    self.handle_telem_error("invalid frame", frame)
+                if(frame[-1:][0] != self._transmittion_constants['FOOTER']):
+                    self._log_queue.put("invalid frame")
                 else:
-                    self.telem_frame_queue.put(frame)
-                    self.current_telem_frame[:] = frame
+                    self._telem_frame_queue.put(frame)
+                    self._current_telem_frame[:] = frame
+                    self.handled_most_recent.value = 0
                     
 
-            if(self._bytes_received[0:4] == self._STRING_HEADER and len(self._bytes_received) >= 9):
+            if(self._bytes_received[0:4] == self._transmittion_constants['STRING_HEADER'] and len(self._bytes_received) >= 9):
                 byte_after_header = bytearray(self._bytes_received[4:5]) #first four bytes are header, 5th byte is size
                 length = unpack('=B', byte_after_header)[0]
 
@@ -100,22 +135,12 @@ class RF():
                     string_with_struct = '=' + str(length) + 'cI'
                     parsedString = unpack(string_with_struct, incomingString)
                     
-                    if(parsedString[-1:][0] != self._FOOTER):
-                        self.handle_string_error("Invalid String, no footer" + str(parsedString))
+                    if(parsedString[-1:][0] != self._transmittion_constants['FOOTER']):
+                        self._log_queue.put("Invalid String, no footer" + str(parsedString))
                     else:
                         try:
                             byte_chars = [s.decode() for s in parsedString[:-1]]
                             output_string = "".join(byte_chars)
-                            self.handle_string_received(output_string)
+                            self._log_queue.put(output_string)
                         except:
-                            self.handle_string_error("Character value exceeds ascii values" + str(parsedString))
-
-
-    def arm(self):
-        self._comport.write(bytes('arm\n', 'utf8'))
-
-    def abort(self):
-        self._comport.write(bytes('abort\n', 'utf8'))
-
-    def is_open(self):
-        return self._comport.isOpen()
+                            self._log_queue.put("Character value exceeds ascii values" + str(parsedString))
