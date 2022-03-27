d570c0ef8282eb23542d26adac2817e0e4143b00
gabekole <gabriel.koleszar@uconn.edu>
2022-03-27 14:48:57 -0400

    Add new buttons for connection

--- live_monitoring/commanding_panel.py
+++ live_monitoring/commanding_panel.py
@@ -17,23 +17,28 @@ class commanding_pannel(QtWidgets.QLabel):
     def setup_gui(self):
         self.command = QtWidgets.QLineEdit()
         self.command.setPlaceholderText("Enter Command")
-        self.layout.addWidget(self.command, 0, 0)
+        self.layout.addWidget(self.command, 0, 0, 1, 2)
 
         self.send_command_button = QtWidgets.QPushButton("Send Command")
-        self.layout.addWidget(self.send_command_button, 1, 0)
+        self.layout.addWidget(self.send_command_button, 1, 0, 1, 2)
 
         self.abort_button= QtWidgets.QPushButton("ABORT")
         self.abort_button.setStyleSheet("background: red; color: white; font-size: 13px;")
-        self.layout.addWidget(self.abort_button, 2, 0)
+        self.layout.addWidget(self.abort_button, 2, 0, 1, 2)
 
-        self.countdown_button = QtWidgets.QPushButton("GO FOR COUNTDOWN")
+        self.countdown_button = QtWidgets.QPushButton("Go to countdown")
         self.countdown_button.setStyleSheet("background: lime; font-size: 13px;")
-        self.layout.addWidget(self.countdown_button, 3, 0)
+        self.layout.addWidget(self.countdown_button, 3, 1)
+
+        self.standby_button = QtWidgets.QPushButton("Go to standby")
+        self.standby_button.setStyleSheet("background: lime; font-size: 13px;")
+        self.layout.addWidget(self.standby_button, 3, 0)
 
     def connect_functionality(self):
         self.command.returnPressed.connect(self.send_command_and_clear_text)
         self.send_command_button.clicked.connect(self.send_command_and_clear_text)
-        self.countdown_button.clicked.connect(lambda: self.send_command("COMMAND: set_mode 1"))
+        self.countdown_button.clicked.connect(lambda: self.send_command("COMMAND: standby_to_countdown"))
+        self.standby_button.clicked.connect(lambda: self.send_command("COMMAND: idle_to_standby"))
         self.abort_button.clicked.connect(lambda : self.send_command("COMMAND: ABORT"))
 
 
